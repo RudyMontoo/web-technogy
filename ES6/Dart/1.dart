1611ad500ddff416ec7387558a700a7a2d88601a
RudyMontoo <rudraeng27@gmail.com>
2026-02-18 14:03:27 +0530

    started learning dart with datatype

---
+++ ES6/Dart/1.dart
@@ -0,0 +1,8 @@
+void main(){
+  var a="Hello";
+  var b=10;
+  var c=10.10;
+  print(a);
+  print(b.runtimeType);
+  print(c.runtimeType);
+}
---
+++ ES6/Dart/2.dart
@@ -0,0 +1,28 @@
+void main(){
+
+  // var a="Hello";
+  // a=10
+  // error if we do like this 
+
+
+// DYNAMIC DATATYPE
+  dynamic b="Hello";
+  b=10;
+  print(b);
+  print(b.length); //run because run time pe fixed hoga
+
+  // Object Datatype
+   dynamic c="Hello";
+  c=10;
+  print(c);
+  print(c.length); //not run because compile time pe fix hoga
+
+
+  var a;
+  a=10;
+  a="Hello";
+  print(a.length);
+
+
+
+}
