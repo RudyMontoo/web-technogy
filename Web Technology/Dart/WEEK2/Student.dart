92989fb2bfa0514f6a964151bbbad029910fde6b
RudyMontoo <rudraeng27@gmail.com>
2026-02-24 12:08:51 +0530

    implemented oops fundamental in dart

---
+++ Web Technology/Dart/WEEK2/Student.dart
@@ -0,0 +1,24 @@
+class Student{
+  String ? name;
+  int ? age;
+  int ? marks;
+  String ? city;
+// parametric constructor
+  Student(this.name, this.age, this.marks, this.city);
+  display(){
+  print("$name $age $marks $city");
+  }
+
+}
+void main(){
+  Student s1=Student("Rudra",30,80,"Sitapur");
+   Student s2=Student("Ram",20,90,"Delhi");
+    Student s3=Student("Mahesh",70,80,"Banaras");
+    List<Student> students=[s1,s2,s3];
+    for(var e in students){
+      e.display();
+    }
+  // print(s1.name);// before using this it give null because its not initializing with given data after this it will work properly
+}
+
+// class -> property + behaviour
---
+++ Web Technology/Dart/WEEK2/main.dart
@@ -0,0 +1,23 @@
+import 'Person.dart';
+
+// void main(){
+//   Person p1=Person("Rudra", 21, "Sitapur");
+//   p1.display();
+// }
+
+
+// encapsulation
+// restrictedd the dirsct acess of class  
+
+// in java it is accessible in class only
+// in dart, private members "starting with _" are private
+// to the file(library), not to the class 
+
+
+void main(){
+  Person p1=Person("Rudra", 21);
+ p1.setCity="Ghaziabad";
+ print(p1.getCity);
+ p1.setBalance=-89;
+ print(p1.getBalance);
+}
